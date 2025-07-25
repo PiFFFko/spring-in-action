8bd4a94a960931640507b4664e1744d6579b1868
PiFFFko <piwen.roman@yandex.ru>
2025-07-25 20:36:47 +0300

    Появилась возможность заказать себе Тако

---
+++ src/main/java/pifffko/taco_cloud/controller/OrderController.java
@@ -0,0 +1,30 @@
+package pifffko.taco_cloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import pifffko.taco_cloud.model.TacoOrder;
+
+@Controller
+@Slf4j
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder tacoOrder, SessionStatus sessionStatus) {
+        log.info("Order submittted: {}", tacoOrder);
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
+
+}
---
+++ src/main/java/pifffko/taco_cloud/model/Taco.java
@@ -0,0 +1,12 @@
+package pifffko.taco_cloud.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+}
